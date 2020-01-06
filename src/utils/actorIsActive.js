bf9a47ae991226bc43bf4c1ba45f39c23588d0e3
jongsung <dazzyjong@gmail.com>
2020-01-06 17:00:10 +0900

    remove log

--- src/utils/actorIsActive.js
+++ src/utils/actorIsActive.js
@@ -2,6 +2,5 @@ export default function actorIsActive(state, type, attrs = {}, from) {
   let node = state.doc.nodeAt(from)
   let ret =  node.hasMarkup(type, attrs)
 
-  console.log(ret)
   return ret
 }
