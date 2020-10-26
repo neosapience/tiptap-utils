517b63247c4a318645a4166a155fa1b7c37c4775
Kang Hyung Won <engen701@gmail.com>
2020-10-26 17:44:22 +0900

    fix: remove console.log

--- src/utils/actorIsActive.js
+++ src/utils/actorIsActive.js
@@ -3,7 +3,6 @@ import nodeIsActive from './nodeIsActive'
 export default function actorIsActive(state, type, attrs = {}, from) {
   if (from !== undefined) {
     let node = state.doc.nodeAt(from)
-    console.log({...node.attrs, ...attrs})
     let ret =  node.hasMarkup(type, {...node.attrs, ...attrs})
     return ret
   } else {
