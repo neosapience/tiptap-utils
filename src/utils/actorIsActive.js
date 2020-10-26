475c8a92fa59b49fe41d0363a4ec1999810a38e5
Kang Hyung Won <engen701@gmail.com>
2020-10-26 17:38:01 +0900

    fix: nodeIsActive with actorIsActive able to multi attribute

--- src/utils/actorIsActive.js
+++ src/utils/actorIsActive.js
@@ -3,7 +3,8 @@ import nodeIsActive from './nodeIsActive'
 export default function actorIsActive(state, type, attrs = {}, from) {
   if (from !== undefined) {
     let node = state.doc.nodeAt(from)
-    let ret =  node.hasMarkup(type, attrs)
+    console.log({...node.attrs, ...attrs})
+    let ret =  node.hasMarkup(type, {...node.attrs, ...attrs})
     return ret
   } else {
     return nodeIsActive(state, type, attrs)
